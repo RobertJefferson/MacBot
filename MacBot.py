0c76da1148a56cce5a300871d6615f525db1a6b8
RobertJefferson <43105118+RobertJefferson@users.noreply.github.com>
2018-09-10 18:02:11 -0500

    Update MacBot.py

--- MacBot.py
+++ MacBot.py
@@ -139,7 +139,8 @@ async def ping(ctx):
 @bot.command(pass_context=True)
 async def rape(ctx, *args):
     mesg = ' '.join(args)
-    return await bot.say(mesg)
+    #return await bot.say(mesg)"The user {0.author.mention} has reported: {1}".format(ctx.message, mesg)
+    return await bot.say("{0.author.mention} has raped: {1}".format(ctx.message, mesg))
     
 
 
